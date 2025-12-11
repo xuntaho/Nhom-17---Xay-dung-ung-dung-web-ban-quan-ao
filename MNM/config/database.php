41ec49ffc4d9c6316239819ac8e050c272a9835f
Nha <nha158400@gmail.com>
2025-12-11 15:55:59 +0700

    da fix loi database

--- MNM/config/database.php
+++ MNM/config/database.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "miussa";
+$dbname = "miu";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
--- MNM/view/index.php
+++ MNM/view/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "config/database.php";
+include "../config/database.php";
 ?>
 <!DOCTYPE html>
 <html lang="vi">
@@ -10,27 +10,19 @@ include "config/database.php";
   <title>MIUSA</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&family=Istok+Web:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="../style/css.css">
 </head>
 
 <body class="body">
 <header class="header">
     <div class="logo">MIU<span>SA</span></div>
     <nav class="menu">
-        <?php include "timkiem.php"; ?>
+        <!-- Tìm kiếm đã bị loại bỏ theo yêu cầu -->
         <a href="index.php"><i class="fa fa-home"></i> Home</a>
-        <a href="giohang.php"><i class="fa-solid fa-cart-shopping"></i> Giỏ hàng</a>
-        <a href="lichsudonhang.php">Lịch sử đơn hàng</a>
-        <a href="about.php">About</a>
-
-        <?php if (!isset($_SESSION["id_nguoi_dung"])) { ?>
-            <a href="dangnhap.php"><i class="fa-solid fa-user"></i> Đăng nhập</a>
-        <?php } else { ?>
-            <span style="color:black;font-size:15px;">
-                Chào bạn <?php echo $_SESSION["ten_dang_nhap"]; ?>
-            </span>
-            <a href="thongtintaikhoan.php"><i class="fa-solid fa-user"></i> Tài khoản</a>
-        <?php } ?>
+        <a href=""><i class="fa-solid fa-cart-shopping"></i> Giỏ hàng</a>
+        <a href="">Lịch sử đơn hàng</a>
+        <a href="">About</a>
+        <a href="dangnhap.php"><i class="fa-solid fa-user"></i> Đăng nhập</a>
     </nav>
 </header>
 
@@ -41,32 +33,30 @@ include "config/database.php";
 <div class="content">
     <div class="product-container" id="products">
       <?php
-      $key = $_GET['search'] ?? '';
+      $sql = "SELECT id_san_pham, ten_san_pham, gia, hinh_anh FROM san_pham ORDER BY id_san_pham ASC";
+      $result = mysqli_query($conn, $sql);
 
-      if ($key !== '') {
-          $key = mysqli_real_escape_string($conn, $key);
-          $sql = "SELECT * FROM san_pham WHERE ten_san_pham LIKE '%$key%'";
-          $result = mysqli_query($conn, $sql);
-      } else {
-          $sql = "SELECT * FROM san_pham ORDER BY id_san_pham ASC";
-          $result = mysqli_query($conn, $sql);
-      }
-
-      while ($row = mysqli_fetch_assoc($result)) {
+      if ($result && mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_assoc($result)) {
+              $id    = (int)$row['id_san_pham'];
+              $name  = htmlspecialchars($row['ten_san_pham'], ENT_QUOTES, 'UTF-8');
+              $price = number_format($row['gia'], 0, ',', '.').'đ';
+              $img   = htmlspecialchars($row['hinh_anh'], ENT_QUOTES, 'UTF-8');
       ?>
-
           <div class="product-card">
-            <a href="chitiet.php?id=<?php echo $row['id_san_pham']; ?>">
-            <img src="../images/<?php echo $row['hinh_anh']; ?>" alt="<?php echo $row['ten_san_pham']; ?>">
-            <?php echo $row['ten_san_pham']; ?>
-            <p class="price"><?php echo number_format($row['gia'], 0, ',', '.'); ?>đ</p>
-            </a>
+            <img src="../images/<?php echo $img; ?>" alt="<?php echo $name; ?>">
+            <div class="product-name"><?php echo $name; ?></div>
+            <p class="price"><?php echo $price; ?></p>
           </div>
-
-      <?php } ?>
-
+      <?php
+          }
+      } else {
+          echo '<p style="padding:20px;">Không có sản phẩm nào.</p>';
+      }
+      ?>
     </div>
 </div>
+
 <footer class="footer">
     <ul class="info">
       <h4 style="font-weight: bold;">HỘ KINH DOANH MIUSA </h4>
@@ -89,10 +79,10 @@ include "config/database.php";
       <h4 style="font-weight: bold;">FANPAGE</h4>
       <li>
         <a href="https://www.facebook.com/share/1GQMPBSu9z/?mibextid=wwXIfr" target="_blank">
-            <img src="../images/fb.png" class="anh">
+            <img src="../images/fb.png" class="anh" alt="facebook">
         </a>
       </li>
-      <li><img src="../images/instagram.png" class="anh"></li>
+      <li><img src="../images/instagram.png" class="anh" alt="instagram"></li>
     </ul>
 </footer>
 </body>
