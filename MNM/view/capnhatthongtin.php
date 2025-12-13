93d04bb89b8ab5d234eec8ceb7c2d7ac1052dee0
Nha <nha158400@gmail.com>
2025-12-13 12:42:28 +0700

    cập nhật thông tin backend

--- MNM/view/capnhatthongtin.php
+++ MNM/view/capnhatthongtin.php
@@ -1,11 +1,44 @@
+<?php
+session_start();
+include "../config/database.php";
+if (!isset($_SESSION['id_nguoi_dung'])) {
+    header("Location: dangnhap.php");
+    exit;
+}
+
+$id = $_SESSION['id_nguoi_dung'];
+$sql = "SELECT * FROM nguoi_dung WHERE id_nguoi_dung = $id";
+$user = mysqli_fetch_assoc(mysqli_query($conn, $sql));
+
+$thongbao = "";
+if (isset($_POST['btn_update'])) {
+
+    $ho_ten  = $_POST['ho_ten'];
+    $so_dt   = $_POST['so_dien_thoai'];
+    $dia_chi = $_POST['dia_chi'];
+
+    $update = "UPDATE nguoi_dung SET 
+                ho_ten = '$ho_ten',
+                so_dien_thoai = '$so_dt',
+                dia_chi = '$dia_chi'
+               WHERE id_nguoi_dung = $id";
+
+    mysqli_query($conn, $update);
+
+    $thongbao = "Cập nhật thông tin thành công!";
+
+    $user = mysqli_fetch_assoc(mysqli_query($conn, $sql));
+}
+?>
 <!DOCTYPE html>
 <html lang="vi">
 <head>
 <meta charset="UTF-8">
-<title>Cập nhật thông tin - MIUSA</title>
+<title>Cập nhật thông tin</title>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&family=Istok+Web&display=swap" rel="stylesheet">
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="../style/css.css">
 
 <style>
@@ -16,7 +49,7 @@
     padding: 30px;
     border-radius: 18px;
     box-shadow: 0 4px 12px rgba(0,0,0,0.2);
-    font-family: 'Istok Web', sans-serif;
+    font-family: 'Istok Web';
 }
 
 .container h1 {
@@ -72,48 +105,47 @@
     <div class="logo">MIU<span>SA</span></div>
     <nav class="menu">
         <input type="text" placeholder="Search..." class="search">
-        <a href="index.html"><i class="fa fa-home"></i> Home</a>
-        <a href="giohang.html"><i class="fa-solid fa-cart-shopping"></i> Giỏ hàng</a>
-        <a href="">Lịch sử đơn hàng</a>
-        <a href="">About</a>
-        <span style="font-size: 15px;">Chào bạn <b>nguyenvana</b></span>
+        <a href="index.php"><i class="fa fa-home"></i> Home</a>
+        <a href="giohang.php"><i class="fa-solid fa-cart-shopping"></i> Giỏ hàng</a>
+        <a href="lichsudonhang.php">Lịch sử đơn hàng</a>
+        <a href="about.php">About</a>
+        <span style="font-size: 15px;">Chào bạn <?= $_SESSION["ten_dang_nhap"]; ?></span>
         <a href="thongtintaikhoan.php"><i class="fa-solid fa-user"></i> Tài khoản</a>
     </nav>
 </header>
 <div class="container">
 
     <h1>Thông tin tài khoản</h1>
-    <p class="thongbao" id="msg" style="display:none;">
-        Cập nhật thông tin thành công!
-    </p>
 
-    <form onsubmit="return saveInfo()">
+    <?php if ($thongbao != "") echo "<p class='thongbao'>$thongbao</p>"; ?>
+
+    <form method="post">
 
         <div class="input-box">
             <label>Họ tên</label>
-            <input type="text" id="ho_ten" value="Nguyễn Văn A" required>
+            <input type="text" name="ho_ten" value="<?= $user['ho_ten']; ?>" required>
         </div>
 
         <div class="input-box">
             <label>Số điện thoại</label>
-            <input type="text" id="so_dien_thoai" value="0909090909">
+            <input type="text" name="so_dien_thoai" value="<?= $user['so_dien_thoai']; ?>">
         </div>
 
         <div class="input-box">
             <label>Địa chỉ</label>
-            <input type="text" id="dia_chi" value="180 Cao Lỗ, TP.HCM">
+            <input type="text" name="dia_chi" value="<?= $user['dia_chi']; ?>">
         </div>
 
-        <button class="btn-save">Lưu thay đổi</button>
+        <button class="btn-save" name="btn_update">Lưu thay đổi</button>
     </form>
 
 </div>
 <footer class="footer">
     <ul class="info">
-      <h4>HỘ KINH DOANH MIUSA</h4>
+      <h4 style="font-weight: bold;">HỘ KINH DOANH MIUSA </h4>
     </ul>
     <ul class="info">
-      <h4>LIÊN KẾT</h4>
+      <h4 style="font-weight: bold;">LIÊN KẾT</h4>
       <li>Chính sách bảo mật</li>
       <li>Hướng dẫn mua hàng</li>
       <li>Chính sách đổi trả</li>
@@ -122,22 +154,16 @@
       <li>Chính sách kiểm hàng</li>
     </ul>
     <ul class="info">
-      <h4>THÔNG TIN LIÊN HỆ</h4>
+      <h4 style="font-weight: bold;">THÔNG TIN LIÊN HỆ</h4>
       <li><i class="fa fa-phone"></i> 0909090909</li>
       <li><i class="fa fa-location-arrow"></i> 180 Cao Lỗ, P. Chánh Hưng, TPHCM</li>
     </ul>
     <ul class="info">
-      <h4>FANPAGE</h4>
+      <h4 style="font-weight: bold;">FANPAGE</h4>
       <li><img src="../images/fb.png" class="anh"></li>
       <li><img src="../images/instagram.png" class="anh"></li>
     </ul>
 </footer>
-<script>
-function saveInfo() {
-    document.getElementById("msg").style.display = "block";
-    return false;
-}
-</script>
 
 </body>
 </html>
