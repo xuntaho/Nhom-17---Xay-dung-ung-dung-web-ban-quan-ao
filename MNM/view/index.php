7f89e57720806e19d5bc9d8ee027d78e00ca52bc
Nha <nha158400@gmail.com>
2025-12-26 18:28:19 +0700

    index va dangnhap

---
+++ MNM/view/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MIUSA - Frontend</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&family=Istok+Web:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../style/css.css">
+</head>
+
+<body class="body">
+<header class="header">
+    <div class="logo">MIU<span>SA</span></div>
+    <nav class="menu">
+        <div class="search-box">
+            <input type="text" class="search" placeholder="Tìm sản phẩm...">
+            <i class="fa fa-search search-icon"></i>
+        </div>
+        <a href="index.php"><i class="fa fa-home"></i> Home</a>
+        <a href=""><i class="fa-solid fa-cart-shopping"></i> Giỏ hàng</a>
+        <a href="">Lịch sử đơn hàng</a>
+        <a href="">About</a>
+        <a href="dangnhap.php"><i class="fa-solid fa-user"></i> Đăng nhập</a>
+    </nav>
+</header>
+<div class="banner">
+  <img src="../images/z7280374913426_f2722e5f205dd34472c815d6aecf61f6.jpg" alt="banner">
+</div>
+<div class="content">
+    <div class="product-container" id="products">
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/sweeter.jpeg" alt="Sản phẩm 1">
+                Sản phẩm 1
+                <p class="price">120.000đ</p>
+            </a>
+        </div>
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/hoodie.jpg" alt="Sản phẩm 1">
+                Sản phẩm 1
+                <p class="price">120.000đ</p>
+            </a>
+        </div>
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/hoodiezip.jpg" alt="Sản phẩm 1">
+                Sản phẩm 1
+                <p class="price">120.000đ</p>
+            </a>
+        </div>
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/quankaki.jpg" alt="Sản phẩm 1">
+                Sản phẩm 1
+                <p class="price">120.000đ</p>
+            </a>
+        </div>
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/quangrey.jpg" alt="Sản phẩm 1">
+                Sản phẩm 1
+                <p class="price">120.000đ</p>
+            </a>
+        </div>
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/vay.png" alt="Sản phẩm 1">
+                Sản phẩm 1
+                <p class="price">120.000đ</p>
+            </a>
+        </div>
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/ryokotee.jpg" alt="Sản phẩm 2">
+                Sản phẩm 2
+                <p class="price">250.000đ</p>
+            </a>
+        </div>
+
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/quancagro.jpg" alt="Sản phẩm 3">
+                Sản phẩm 3
+                <p class="price">99.000đ</p>
+            </a>
+        </div>
+
+        <div class="product-card">
+            <a href="#">
+                <img src="../images/quan.jpg" alt="Sản phẩm 4">
+                Sản phẩm 4
+                <p class="price">350.000đ</p>
+            </a>
+        </div>
+    </div>
+</div>
+<footer class="footer">
+    <ul class="info">
+      <h4>HỘ KINH DOANH MIUSA </h4>
+    </ul>
+
+    <ul class="info">
+      <h4>LIÊN KẾT</h4>
+      <li>Chính sách bảo mật</li>
+      <li>Hướng dẫn mua hàng</li>
+      <li>Chính sách đổi trả</li>
+      <li>Hướng dẫn thanh toán</li>
+      <li>Chính sách vận chuyển</li>
+      <li>Chính sách kiểm hàng</li>
+    </ul>
+
+    <ul class="info">
+      <h4>THÔNG TIN LIÊN HỆ</h4>
+      <li><i class="fa fa-phone"></i> 0909090909</li>
+      <li><i class="fa fa-location-arrow"></i> 180 Cao Lỗ, P. Chánh Hưng, TPHCM</li>
+    </ul>
+
+    <ul class="info">
+      <h4>FANPAGE</h4>
+      <li><img src="../images/fb.png" class="anh"></li>
+      <li><img src="../images/instagram.png" class="anh"></li>
+    </ul>
+</footer>
+
+</body>
+</html>
